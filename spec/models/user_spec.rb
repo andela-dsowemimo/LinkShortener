ff18ae6516792bb0bb86a5b09ce89f65e6601be5
Daisi Sowemimo <daisi.sowemimo@andela.com>
2015-09-10 14:47:15 +0100

    Add validations to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,17 @@ RSpec.describe User, type: :model do
     expect(build :user).to be_valid
   end
 
+  it "is invalid without a name"  do
+    expect(build(:user, name: nil)).to have(1).errors_on(:name)
+  end
+
+  it "is invalid without an email" do
+    expect(build(:user, email: nil)).to have(1).errors_on(:email)
+  end
+
   it "is invalid witn a duplicate email" do
+    create(:user)
     temi = build(:user, name: "Temi Olaolu", email: "daisi.sowemimo@yahoo.com")
-    # expect(temi).to have(1).errors_on(:email)
+    expect(temi).to have(1).errors_on(:email)
   end
 end
