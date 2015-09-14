3f1280b93ccad35ed0c87f927ea6fb3ad8bd8895
Daisi Sowemimo <daisi.sowemimo@andela.com>
2015-09-14 11:00:26 +0100

    Add javascript to links and links partial

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,13 +1,34 @@
 require 'securerandom'
 
 class LinksController < ApplicationController
+  def index
+    @link = Link.new
+    @links = Link.all
+  end
+
   def new
     @link = Link.new
   end
 
   def create
+    # require 'pry'; binding.pry
     @link = Link.new(link_params)
-    @link.shortened_link_address = SecureRandom.urlsafe_base64(20)
+    @link.shortened_link_address = SecureRandom.urlsafe_base64(8)
+    if @link.save
+      respond_to do |format|
+        format.html {}
+        format.js {}
+      end
+    end
+  end
+
+  def show
+
+  end
+
+  def visit_webpage
+    @link = Link.find_by(shortened_link_address: params[:shortened_link_address])
+    redirect_to @link.full_link_address
   end
 
   private
